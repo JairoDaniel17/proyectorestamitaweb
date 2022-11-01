48b370c570145776ced880edc530f2317a6f399e
jairo <jairodaniel.mt@gmail.com>
2022-11-01 08:24:17 -0500

    primario

---
+++ src/main/java/mvc/ProyectoRestamitaApplication.java
@@ -0,0 +1,13 @@
+package mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoRestamitaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoRestamitaApplication.class, args);
+    }
+
+}
